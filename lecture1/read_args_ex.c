1d731eccad6e436b0c687104a49b528b8774cec1
Christian DeLozier <crdelozier@gmail.com>
2015-01-10 13:46:33 -0500

    Adding lecture 1 notes and examples

---
+++ lecture1/read_args_ex.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  if (argc == 2) {
+    int val = atoi(argv[1]);
+    if (val == 100) {
+      printf("That's a good number!\n");
+    } else {
+      printf("%d is a bad number! Do not enter %d!\n", val, val);
+    }
+  }
+  return 0;
+}
